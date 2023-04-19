2f37769c8cb452770ca5576e531e41049360ef1b
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2023-04-19 13:36:41 +0200

    begin opzet server

---
+++ server.js
@@ -0,0 +1,43 @@
+import express, { request } from "express"
+
+const server = express()
+
+server.use(express.static("public"))
+server.set("view engine", "ejs")
+server.set("views", "./views")
+
+//routes 
+
+function Routemaker(x,y){
+server.get(x, (request, response) =>{
+    response.render(y)
+})
+}
+
+server.get('/afbeelding', (request, response) =>{
+    response.render('afbeelding')
+})
+
+Routemaker('/', 'index')
+// Routemaker('/afbeelding', 'afbeelding')
+Routemaker('/animatie', 'animatie')
+Routemaker('/berichtgeving', 'berichtgeving')
+Routemaker('/blokken', 'blokken')
+Routemaker('/formulieren', 'formulieren')
+Routemaker('/globals', 'globals')
+Routemaker('/headings', 'headings')
+Routemaker('/iconen', 'iconen')
+Routemaker('/interactieve-componenten', 'interactieve-componenten')
+Routemaker('/kleuren', 'kleuren')
+Routemaker('/knoppen', 'knoppen')
+Routemaker('/lijsten', 'lijsten')
+Routemaker('/media', 'media')
+Routemaker('/reclame', 'reclame')
+
+//poortnummer instellen
+server.set("port", 8800)
+
+//start de server
+server.listen(server.get("port"), () => {
+  console.log(`Application started on http://localhost:${server.get("port")}`)
+})
